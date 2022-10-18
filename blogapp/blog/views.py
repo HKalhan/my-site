acb0169b5a375dece7582249968a0edb8fd4d213
HKalhan <hasret.kalhann@gmail.com>
2022-10-18 15:05:26 +0300

    views&url

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("home page")
+
+def blogs(request):
+    return HttpResponse("blog page")
